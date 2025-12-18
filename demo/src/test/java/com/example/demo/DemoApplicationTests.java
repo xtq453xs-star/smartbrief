3bb0c8ff221ae1cb34cd6fc393ea81d4ae312f90
xtq453xs-star <xtq453xs@gmail.com>
2025-12-19 01:22:04 +0900

    Add AOZORA_R2DBC_URL to test properties

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -11,7 +11,8 @@ import org.springframework.test.context.TestPropertySource;
     "SPRING_R2DBC_PASSWORD=password",
     "JWT_SECRET_KEY=test-secret-key-for-testing-purpose-only",
     "JWT_EXPIRATION=3600000",
-    "STRIPE_API_KEY=test-stripe-api-key-for-testing-only"
+    "STRIPE_API_KEY=test-stripe-api-key-for-testing-only",
+    "AOZORA_R2DBC_URL=r2dbc:mysql://localhost:3306/aozora"
 })
 class DemoApplicationTests {
 
