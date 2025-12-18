66a3964ad1e4359c6a79c3c295770dfa6c924b87
xtq453xs-star <xtq453xs@gmail.com>
2025-12-19 01:09:50 +0900

    Add test properties for R2DBC database configuration

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "SPRING_R2DBC_URL=r2dbc:mysql://localhost:3306/test",
+    "SPRING_R2DBC_USERNAME=root",
+    "SPRING_R2DBC_PASSWORD=password"
+})
 class DemoApplicationTests {
 
 	@Test
